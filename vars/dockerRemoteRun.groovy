5b27fa1318a3710a20164ebb32cb5dce2914241d
Moses Kabungo <moses.kabungo@tz.baridibaridi.com>
2022-06-02 16:53:08 +0300

    initial commit with basic scripts

---
+++ vars/dockerRemoteRun.groovy
@@ -0,0 +1,17 @@
+
+// Run docker image to the remote host.
+def dockerRemoteRun(Map config = [:]) {
+  sh ('''#!/usr/bin/env bash
+    # Stop previously launched container by image name (Works in docker 1.9+)
+    docker --host ${config.sshUrl} ps -aqf "name=${config.appName}" \
+        | xargs -r docker --host ${config.sshUrl} stop
+    docker --host ${config.sshUrl} run \
+        --env-file ${config.env}
+        --name {config.appName} \
+        --publish ${config.bindPort}:${config.containerPort} \
+        --detach \
+        --rm \
+        --privileged \
+    ${config.img}
+  ''')
+}
---
+++ vars/dockerRemoteSave.groovy
@@ -0,0 +1,10 @@
+// Push docker image to the remote host via SSH
+def dockerRemoteSave(Map config = [:]) {
+  sshagent(credentials: [credentialsId]) {
+    sh'''
+      [ -d ~/.ssh ] || mkdir ~/.ssh
+      ssh-keyscan -t rsa,dsa ${host} >> ~/.ssh/known_hosts
+      docker save ${config.img} | ssh -C ${config.user}@${config.host} docker load
+    '''
+  }
+}
