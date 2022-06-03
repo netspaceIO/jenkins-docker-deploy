f8300b198fcc49ea9981e9113253f78274003816
Moses Kabungo <moses.kabungo@tz.baridibaridi.com>
2022-06-03 11:49:43 +0300

    replace single with double quotes

--- vars/dockerRemoteSave.groovy
+++ vars/dockerRemoteSave.groovy
@@ -1,10 +1,10 @@
 // Push docker image to the remote host via SSH
 def call(Map config = [:]) {
   sshagent(credentials: [config.credentialsId]) {
-    sh'''
+    sh"""
       [ -d ~/.ssh ] || mkdir ~/.ssh
       ssh-keyscan -t rsa,dsa ${config.host} >> ~/.ssh/known_hosts
       docker save ${config.img} | ssh -C ${config.user}@${config.host} docker load
-    '''
+    """
   }
 }
