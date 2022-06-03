d4f4b0be377b9a4c0c8f8dca834c4751f4bbcfd4
Moses Kabungo <moses.kabungo@tz.baridibaridi.com>
2022-06-03 15:22:17 +0300

    externalize dockerRemoveSave.groovy to a different script

--- vars/dockerRemoteSave.groovy
+++ vars/dockerRemoteSave.groovy
@@ -1,10 +1,12 @@
 // Push docker image to the remote host via SSH
 def call(Map config = [:]) {
+  loadShellScript(name: 'remote-save-docker-img.sh')
+  // Execute the script within
   sshagent(credentials: [config.credentialsId]) {
-    sh"""
-      [ -d ~/.ssh ] || mkdir ~/.ssh
-      ssh-keyscan -t rsa,dsa ${config.host} >> ~/.ssh/known_hosts
-      docker save ${config.img} | ssh -C ${config.user}@${config.host} docker load
-    """
+    sh """
+      ./remote-save-docker-img.sh \
+        -t ${config.host} \
+        -u ${config.user} \
+      ${config.img}"""
   }
 }
