f1cb5b5106deb4d8e148133496b6d6b23f3503d0
Moses Kabungo <moses.kabungo@tz.baridibaridi.com>
2022-06-03 15:42:32 +0300

    add debug messages

--- vars/loadShellScript.groovy
+++ vars/loadShellScript.groovy
@@ -1,5 +1,6 @@
 
 def call(Map config = [:]) {
+  echo "Writing script..."
   def scriptContents = libraryResource "/io/netspace/${config.name}"
   writeFile file: "${config.name}", text: scriptContents
   sh "chmod a+x ./${config.name}"
