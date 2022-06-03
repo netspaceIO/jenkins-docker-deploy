7c6d9b89f05e1a2b13db1dac7ad5ebea65c8c4ef
Moses Kabungo <moses.kabungo@tz.baridibaridi.com>
2022-06-03 16:07:26 +0300

    remove absolute path

--- vars/loadShellScript.groovy
+++ vars/loadShellScript.groovy
@@ -1,7 +1,7 @@
 
 def call(Map config = [:]) {
   echo "Writing script..."
-  def scriptContents = libraryResource "/resources/io/netspace/${config.name}"
+  def scriptContents = libraryResource "${config.name}"
   writeFile file: "${config.name}", text: scriptContents
   sh "chmod a+x ./${config.name}"
 }
